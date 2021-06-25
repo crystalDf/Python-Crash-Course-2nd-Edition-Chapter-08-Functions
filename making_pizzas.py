dc7a0a036b37b182c64dd702debc921315a0f817
crystalDf <chendong333@gmail.com>
2021-06-25 12:47:44 +0800

    Chapter 08

---
+++ making_pizzas.py
@@ -0,0 +1,17 @@
+import pizza
+import pizza as p
+from pizza import *
+from pizza import make_pizza
+from pizza import make_pizza as mp
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
